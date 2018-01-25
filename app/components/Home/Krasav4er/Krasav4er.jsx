7a4dc2dda6268add480ba43bde4229c7b4a04688
vipslord <my.yershov@gmail.com>
2018-01-25 18:20:20 +0200

    test

--- app/components/Home/Krasav4er/Krasav4er.jsx
+++ app/components/Home/Krasav4er/Krasav4er.jsx
@@ -10,6 +10,7 @@ class Krasav4er extends Component {
         <Link to={'/html/Krasav4er/project1'}>Project #1</Link><br />
         <Link to={'/html/Krasav4er/project2'}>Project #2</Link><br />
         <Link to={'/html/Krasav4er/project3'}>Project #3</Link>
+        test
         <div className='test' />
       </div>
     )
